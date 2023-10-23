cc6a1ba15265de2978622bf4dbc9e8245b9713a6
Sebastian Pokladek <sebpok228@gmail.com>
2023-10-23 09:08:37 +0200

    ded

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Bitcoin } from '../assets/icons/bitcoin-logo-svgrepo-com.svg';
+
+const crypto = {
+
+};
+
+const Wrapper = styled.div`
+`;
 
 function Card() {
   return (
-    <div>
+    <Wrapper>
+      <div>
+        <Bitcoin />
+      </div>
+      <h3>Bitcoin</h3>
       <div />
-    </div>
+    </Wrapper>
   );
 }
 
